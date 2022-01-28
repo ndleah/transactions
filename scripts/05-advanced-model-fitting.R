73bf750cdcad4dd7841157160b63bde0be9eb025
Leah Nguyen <nduongthucanh@gmail.com>
2022-01-28 14:14:26 +0700

    Update 05-advanced-model-fitting.R

--- scripts/05-advanced-model-fitting.R
+++ scripts/05-advanced-model-fitting.R
@@ -14,116 +14,112 @@
 library(modelr)       # computing regression model performance metrics
 library(caret)        # streamline the model training process
 library(xts)          # convert df to ts object
-library(RcppRoll)     # used to calculate rolling mean
-library(broom)        
-#### function ####
+library(tidyverse)    # data wrangling
+library(dplyr)        # data wrangling
+library(hydroGOF)     # Goodness-of-Fit Functions for Comparison
 
-model_summary <- function(mod){
-  mod_summary <- list()
-  
-  mod_summary$r2 <- summary(mod)$adj.r.squared
-  mod_summary$rse <- summary(mod)$sigma
-  mod_summary$aug <- mod %>% augment()
-  # calculate RMSE
-  mod_summary$RMSE <- sqrt(mean(mod_summary$aug$.resid ^ 2))
-  
-  #inspect RSE, Adj R-squared
-  # 
-  # sprintf("RMSE: %0.3f", mod_summary$RMSE)
-  # sprintf("RSE: %0.4f", mod_summary$rse)
-  # sprintf("Adj R-sqr: %0.4f", mod_summary$r2) 
-  print(paste0("Adj R-sqr: ", mod_summary$r2))
-  print(paste0("RMSE: ", mod_summary$RMSE)) 
-  # or RMSE(pred = mod1$fitted.values, obs = mod1$model$monthly_mean)
-  print(paste0("RSE: ", mod_summary$rse))
-  
-  
-}
-
-# print RMSE, RSE and AdjR2 for model
-# cross validate model with out-of-sample and print average out-of-sample RMSE 
-fit_model_cv <- function (df, formula, ind=1, loc=1){
-  df_subset <- df %>% filter(industry==ind, location==loc)
-  trControl <- trainControl(method = "cv",number = 15, verboseIter = FALSE)
-  mod <- train(formula, df_subset, method = "lm", trControl = trControl)
-  
-  print(formula)
-  print("cross validation")
-  print(mod$results)
-  print("final model")
-  model_summary(mod$finalModel)
-  return(mod)
-}
-
-glimpse(aggregated_transactions)
-# create a model per combo, add the model to a list so it can later be used to
-# predict out of sample for all combos
-combos <- aggregated_transactions %>% select(industry,location) %>% distinct()
-
+# convert variables to factor
 aggregated_transactions$industry <- as.factor(aggregated_transactions$industry)
 aggregated_transactions$location <- as.factor(aggregated_transactions$location)
 aggregated_transactions$month_number <- as.factor(aggregated_transactions$month_number)
-aggregated_transactions$year_number <- as.factor(aggregated_transactions$year_number)
+aggregated_transactions$year_number <- as.numeric(aggregated_transactions$year_number)
+
+
+month_number <- as.factor(12)
+year_number <- 2016
+
+
+# Create a dataframe containing just the December 2016 data
+dec_2016 <- data.frame(year_number, month_number)
+
 
 # initalise objects to hold fitted model and summary performance results
-mods_all <- list()
-mods_df <- data_frame()
+industry <- list()
+location <- list()
+R_Square <- list()
+Adjusted_R_Square <- list()
+RMSE_Error <- list()
+pred_dec_2016 <- list()
 
-# loop to fit models for all industry locations 
-for (i in 1:nrow(combos)) {
-  c <- combos[i,]
-  print(c)
-  #cross validate the model
-  mod <- fit_model_cv(aggregated_transactions, ind = c$industry, loc=c$location, 
-                      formula = monthly_amount ~ year_number + month_number)
-  
-  #add the final model to list of trained models
-  mods_all[[i]] <- list(industry = c$industry,
-                        location = c$location, 
-                        model = mod, 
-                        RMSE = RMSE(pred = mod$finalModel$fitted.values,
-                                    obs = mod$finalModel$model$.outcome), 
-                        AdjR2 = summary(mod$finalModel)$adj.r.squared, 
-                        RMSE.cv = mod$results$RMSE, 
-                        AdjR2.cv = mod$results$Rsquared)
-  
-  #add stats to dataframe of all models 
-  mods_df <- rbind( mods_df, 
-                    data_frame(industry = c$industry,
-                               location = c$location,
-                               RMSE = RMSE(pred = mod$finalModel$fitted.values,
-                                           obs = mod$finalModel$model$.outcome), 
-                               AdjR2 = summary(mod$finalModel)$adj.r.squared, 
-                               RMSE.cv = mod$results$RMSE, 
-                               AdjR2.cv = mod$results$Rsquared, 
-                               month_number = factor(12), 
-                               year_number = 2016))
-  
+
+# create a loop to build model that loop through all industries and locations, 
+# then calculate the evaluation metrics for each
+for(i in 1:10)
+{
+  for(j in 1:10)
+  {
+    test <- aggregated_transactions %>% 
+      filter(industry==i & location==j)
+    
+    if(nrow(test) > 15)
+    {
+      # Row counts to check
+      ind = ceiling(nrow(test[test$date < "2016-01-01",]))
+      train_ind <- seq_len(ind)
+      
+      # create train-test split
+      trainset <- test[train_ind,]
+      testset <- test[-train_ind,]
+      
+      
+      # train the model
+      # Model 1: monthly_amount ~ date + month_number
+      data.lm1 = lm(formula = monthly_amount ~ month_number, data = trainset)
+      
+      # Model 2: monthly_amount ~ date + month_number + year_number
+      data.lm2 = lm(formula = monthly_amount ~ year_number + month_number, data = trainset)
+      
+      # # Model 3: monthly_amount ~ date ~ month_number
+      # data.lm3 = lm(formula = monthly_amount ~ year_number + month_number, data = trainset)
+      
+       
+      
+      # Output prediction based on the test set
+      a <- predict(data.lm, testset)
+      
+      # Get the last prediction value (which is the Dec 2016 value)
+      b <- predict(data.lm, dec_2016)
+      
+      
+      # store the prediction output as a list
+      test_pred_add1 <- list(a)
+      
+      # convert the list into a data frame
+      test_prediction1 <- as.data.frame(test_pred_add1)
+      
+      # rename the data frame first column
+      names(test_prediction1)[1] <- "Test_Prediction" 
+      
+      # combine the df with the test data set
+      test_actual_pred1 <- cbind(testset , test_prediction1)
+      
+      # calculate the RMSE for each of the observation
+      rmse <- hydroGOF::rmse(test_actual_pred1$Test_Prediction, test_actual_pred1$monthly_amount)
+      
+      
+      # append all results into the predicted df 
+      industry <- append(industry , i)
+      location <- append(location , j)
+    
+      # Calculate R Square
+      R_Square <- append(R_Square , summary(data.lm)$r.squared)
+      
+      # Calculate RMSE
+      RMSE_Error <- append(RMSE_Error , rmse)
+      
+      # Calculate Adjusted R Square
+      Adjusted_R_Square <- append(Adjusted_R_Square , summary(data.lm)$adj.r.squared)
+      
+      # Predict the December 2016 transaction amount
+      pred_dec_2016 <- append(pred_dec_2016 , b)
+    }
+    
+  }
 }
 
+yan <- as.data.frame(do.call(rbind, Map(data.frame, A=industry, B=location , C=R_Square, D=Adjusted_R_Square, E = RMSE_Error, F = pred_dec_2016))) 
 
+# formatting the column names to justify the information for each column
+colnames(yan) <- c("industry", "location", "R_Square", "Adjusted_R_Square","RMSE","Dec_2016_Pred")
+arrange(yan, Adjusted_R_Square)
 
-#### predict dec 2016 for all industries and locations ####
-mods_df <- mods_df %>% mutate(monthly_amount = NA)
-
-#genereate predictions for all models 
-for (i in 1:nrow(mods_df)) {
-  #retrieve model from mods list 
-  mod <- mods_all[[i]]
-  #prep input for prediction 
-  input <- mods_df[i,] %>% select(year,month)
-  #save prediction back to mods_df monthly_amount coln
-  mods_df[i,]$monthly_amount <- predict(mod$model, input)
-  
-}
-
-
-#### save all predictions to file ####
-# Prepare a data set for all predictions
-pred_all <- mods_df %>% 
-  mutate(date = dmy(paste("01", as.character(month), as.integer(year), sep = "-"))) %>% 
-  select(date, industry, location, monthly_amount)
-
-
-# write to file 
-write_csv(pred_all, "./transactions_dec2016.csv")
